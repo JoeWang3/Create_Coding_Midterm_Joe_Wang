9ee8546caa28df55fdd473f480ffa524ee164059
JoeWang3 <yw5970@nyu.edu>
2024-03-12 16:43:35 -0400

    draw sunflower
    implement a sunflower function

--- sketch.js
+++ sketch.js
@@ -1,85 +1,50 @@
-let frogX, frogY;
-let frogAngle;
-
 function setup() {
   createCanvas(800, 800);
-  frogAngle = 0;
-  frogX = width/2;
-  frogY = height/2;
 }
 
 function draw() {
   background(255);
-  push();
-    translate(frogX, frogY);
-    rotate(radians(frogAngle));
-    scale(5, 5);
-    translate(-10, -7);
-    drawfrog();
-  pop();
-  drawfrog();
+  sunFlower(200, 200);
 }
 
-function keyTyped(){
-  if(key == 'w'){
-    frogY -= 10;
-    frogAngle = 0;
-  }
-  if(key == 'd'){
-    frogX += 10;
-    frogAngle = 90;
-  }
-  if(key == 's'){
-    frogY += 10;
-    frogAngle = 180;
-  }
-  if(key == 'a'){
-    frogX -= 10;
-    frogAngle = 270;
+function drawGrid(x, y, r) {
+  let spacing = 10; 
+  stroke(0); 
+  for (let i = x - r; i < x + r; i += spacing) {
+    for (let j = y - r; j < y + r; j += spacing) {
+      if (dist(x, y, i, j) <= r) {
+        strokeWeight(2);
+        point(i, j);
+      }
+    }
   }
 }
 
-function drawfrog() {
-  noStroke();
-  fill(32, 219, 36);
-  beginShape();
-  vertex(9, 1);
-  vertex(13, 1);
-  vertex(14, 4);
-  vertex(17, 6);
-  vertex(18, 5);
-  vertex(18, 1);
-  vertex(21, 3);
-  vertex(19, 4);
-  vertex(19, 8);
-  vertex(16, 7);
-  vertex(19, 9);
-  vertex(20, 10);
-  vertex(21, 14);
-  vertex(18, 15);
-  vertex(18, 11);
-  vertex(15, 10);
-  vertex(13, 13);
-  vertex(8, 13);
-  vertex(7, 11);
-  vertex(5, 10);
-  vertex(4, 15);
-  vertex(1, 13);
-  vertex(3, 11);
-  vertex(4, 8);
-  vertex(6, 9);
-  vertex(6, 7);
-  vertex(3, 7);
-  vertex(3, 4);
-  vertex(1, 3);
-  vertex(5, 1);
-  vertex(4, 6);
-  vertex(8, 4);
-  vertex(9, 1);
-  endShape();
-  fill(248, 235, 21);
-  ellipse(11, 7, 2, 2);
-  fill(253, 3, 217);
-  ellipse(7, 3, 2, 2);
-  ellipse(15, 3, 2, 2);
+function sunFlower(x_co, y_co){
+
+  stroke(0);
+  line(x_co, y_co+80, x_co, y_co+200);
+
+  let i = 4;
+  for (let angle = 0; angle < TWO_PI; angle += PI / 8) {
+    let x = x_co + cos(angle) * 60;
+    let y = y_co + sin(angle) * 60;
+    push();
+    translate(x, y);
+    rotate(radians(i*22.5));
+    ellipse(0, 0, 20, 40);
+    pop();
+    i++;
+  }
+
+  fill(255);
+  circle(x_co, y_co, 110);
+
+  ellipse(x_co-20, y_co+120, 40, 20);
+  ellipse(x_co+20, y_co+120, 40, 20);
+
+  drawGrid(x_co, y_co, 55);
+
 }
+
+
