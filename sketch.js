31bb8588b5e8b3ba40f0bbf14f32782d12ed700f
JoeWang3 <yw5970@nyu.edu>
2024-03-13 23:49:13 -0400

    add sunflower class
    add sunFlower class and use it in the draw loop

--- sketch.js
+++ sketch.js
@@ -1,10 +1,22 @@
+let sunFlowers = [];
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1200, 800);
 }
 
 function draw() {
   background(255);
-  sunFlower(200, 200);
+  for(let i = 100; i < 1200; i += 200){
+    for(let j = 100; j < 800; j += 250){
+      let sun_flower = new Sunflower(i, j, 1);
+      sunFlowers.push(sun_flower);
+    }
+  }
+  for(let i = 0; i < sunFlowers.length; i++){
+    sunFlowers[i].display();
+  }
+
+
+
 }
 
 function drawGrid(x, y, r) {
@@ -20,31 +32,42 @@ function drawGrid(x, y, r) {
   }
 }
 
-function sunFlower(x_co, y_co){
 
-  stroke(0);
-  line(x_co, y_co+80, x_co, y_co+200);
-
-  let i = 4;
-  for (let angle = 0; angle < TWO_PI; angle += PI / 8) {
-    let x = x_co + cos(angle) * 60;
-    let y = y_co + sin(angle) * 60;
-    push();
-    translate(x, y);
-    rotate(radians(i*22.5));
-    ellipse(0, 0, 20, 40);
-    pop();
-    i++;
+class Sunflower{
+  constructor(x_co, y_co, scale){
+    this.x_co = x_co;
+    this.y_co = y_co;
+    this.scale = scale;
   }
+  display(){
+    stroke(0);
+    line(this.x_co, this.y_co+80, this.x_co, this.y_co+200);
 
-  fill(255);
-  circle(x_co, y_co, 110);
+    noStroke();
+    fill(color(252, 207, 49));
+    let i = 4;
+    for (let angle = 0; angle < TWO_PI; angle += PI / 8) {
+      let x = this.x_co + cos(angle) * 60;
+      let y = this.y_co + sin(angle) * 60;
+      push();
+      translate(x, y);
+      rotate(radians(i*22.5));
+      ellipse(0, 0, 20, 40);
+      pop();
+      i++;
+    }
 
-  ellipse(x_co-20, y_co+120, 40, 20);
-  ellipse(x_co+20, y_co+120, 40, 20);
+    fill(255);
+    circle(this.x_co, this.y_co, 110);
 
-  drawGrid(x_co, y_co, 55);
+    stroke(0);
+    fill(color(160, 254, 101));
+    ellipse(this.x_co-20, this.y_co+120, 40, 20);
+    ellipse(this.x_co+20, this.y_co+120, 40, 20);
 
+    drawGrid(this.x_co, this.y_co, 55);
+
+  }
+  
 }
-
 
